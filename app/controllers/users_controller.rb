e97283b164cd3f2a094f634afe4904d7438d9bd3
clare <chblackburne@gmail.com>
2018-09-17 11:59:16 +0100

    Handles successfull and unsuccessful form submission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # do sth
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
     else
       render 'new'
     end
