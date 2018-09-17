40ea39893de89302c48f026c8d0d260bde9ca470
clare <chblackburne@gmail.com>
2018-09-17 13:59:29 +0100

    Handles invalid login attempt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       #log user in and redirect to show page
     else
-      flash[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
