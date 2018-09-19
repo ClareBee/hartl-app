616dc96d5f313d5fe9f9c0f0817e5afef485f1b2
Clare Blackburne <clare.blackburne@castlightfinancial.com>
2018-09-19 17:25:34 +0100

    Adds another user to fixtures to test user can only edit own details

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,6 +8,11 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
+
+  def current_user?(user)
+    user == current_user
+  end
+  
   def current_user
     # note that this is an assignment
     if (user_id = session[:user_id])
