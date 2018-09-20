47f0df57e4c56cb41e7716bb3c2324a597fb8476
Clare Blackburne <clare.blackburne@castlightfinancial.com>
2018-09-20 12:51:06 +0100

    Adds user index page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,6 +8,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    log_in_as users(:michael)
+    get root_path
+    assert_select "a[href=?]", logout_path
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
