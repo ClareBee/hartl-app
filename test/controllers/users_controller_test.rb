c63d1594d206ef419809b8b84664f2dcda1674ce
Clare Blackburne <clare.blackburne@castlightfinancial.com>
2018-09-21 10:22:39 +0100

    Adds admin ability to destroy users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -48,6 +48,27 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not @other_user.admin?
     patch user_path(@other_user), params: { user: { password: 'foobar', password_confirmation: 'foobar', admin: true } }
     assert_not @other_user.reload.admin?
-    debugger
+  end
+
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect destroy when logged in as a non-admin' do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
+
+  test 'should destroy user record when logged in as admin' do
+    log_in_as(@user)
+    assert_difference 'User.count', -1 do
+      delete user_path(@other_user)
+    end
   end
 end
