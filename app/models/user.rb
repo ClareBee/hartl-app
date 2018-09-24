e9231d52d07b28188b6281c03d6e9c66737f017e
Clare Blackburne <clare.blackburne@castlightfinancial.com>
2018-09-24 09:05:51 +0100

    Sets up activation call back and activates seeds and fixtures

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   before_save { email.downcase! }
+  before_create :create_activation_digest
+
   validates :name, presence: true, length: { maximum: 49 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length:  { maximum: 49 },
@@ -41,5 +43,11 @@ class User < ApplicationRecord
    def forget
      update_attribute(:remember_digest, nil)
    end
+
+   private
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 
 end
