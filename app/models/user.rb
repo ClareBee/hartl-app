36b40ae1f3fc4ced408b5157705444d9fc0bd6da
clare <chblackburne@gmail.com>
2018-10-02 08:56:48 +0100

    Sets up password reset mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email
   before_create :create_activation_digest
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
     # Returns the hash digest of the given string. helps create password for test user
-  def User.digest(string)
+  def self.digest(string)
     # low cost in test, high cost in production
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
@@ -54,6 +54,11 @@ class User < ApplicationRecord
    def send_activation_email
      UserMailer.account_activation(self).deliver_now
    end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
 
    private
     def create_activation_digest
